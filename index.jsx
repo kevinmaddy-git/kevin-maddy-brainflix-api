2e1aed284461c13f5ce3ea601f4d7b09d7ae0243
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-06-28 08:18:11 -0400

    Client side functionality started.  Images not pulling, browser extension needed to override CORS

---
+++ index.jsx
@@ -0,0 +1,19 @@
+const express = require('express');
+const videoRoutes = require('./routes/videos');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/videos', express.static('./public/images'));
+app.use('/videos', videoRoutes);
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
+
--- routes/videos.jsx
+++ routes/videos.jsx
@@ -4,16 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON in request bodies
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Endpoint: GET /videos
 app.get('/videos', (req, res) => {
   const videos = getVideos();
   res.json(videos);
 });
 
-// Endpoint: GET /videos/:id
 app.get('/videos/:id', (req, res) => {
   const videoId = req.params.id;
   const video = getVideoById(videoId);
@@ -24,47 +22,36 @@ app.get('/videos/:id', (req, res) => {
   }
 });
 
-// Endpoint: POST /videos
 app.post('/videos', (req, res) => {
   const videoData = req.body;
   const videos = getVideos();
-
-  // Generate a unique id for the new video
   const newVideoId = generateUniqueId();
 
-  // Add the new video to the array
   const newVideo = {
     id: newVideoId,
     ...videoData,
   };
   videos.push(newVideo);
-
-  // Save the updated array to the JSON file
   saveVideos(videos);
-
   res.status(201).json(newVideo);
 });
 
-// Helper function: Get all videos from the JSON file
 function getVideos() {
   const filePath = path.join(__dirname, '../data/videos.json');
   const jsonData = fs.readFileSync(filePath, 'utf-8');
   return JSON.parse(jsonData);
 }
 
-// Helper function: Get a video by its id
 function getVideoById(id) {
   const videos = getVideos();
   return videos.find((video) => video.id === id);
 }
 
-// Helper function: Save the videos array to the JSON file
 function saveVideos(videos) {
   const filePath = path.join(__dirname, '../data/videos.json');
   fs.writeFileSync(filePath, JSON.stringify(videos, null, 2));
 }
 
-// Helper function: Generate a unique id
 function generateUniqueId() {
   const videos = getVideos();
   if (videos.length === 0) {
@@ -78,3 +65,4 @@ function generateUniqueId() {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
